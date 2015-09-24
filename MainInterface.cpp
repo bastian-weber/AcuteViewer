b111aa3d10fc75cbe4ae0e7b787c288e0f0541e0
Bastian Weber <17008429+bastian-weber@users.noreply.github.com>
2015-09-24 15:38:38 +0200

    Updated zoom centering so it also works when image rotated (only when using scroll wheel).

--- MainInterface.cpp
+++ MainInterface.cpp
@@ -9,7 +9,7 @@ namespace sv {
 		this->imageView = new hb::ImageView(this);
 		this->imageView->setShowInterfaceOutline(false);
 		this->imageView->setUseSmoothTransform(false);
-		//this->imageView->setRotation(30);
+		this->imageView->setRotation(270);
 		setCentralWidget(this->imageView);
 	}
 
