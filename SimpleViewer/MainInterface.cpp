e9f4b227654f43b4ee1edd267182222b429618c8
Bastian Weber <bastian.weber@uni-weimar.de>
2015-10-20 09:48:02 +0200

    Updated preference handling (ini files in program directory are a problem on Windows if program directory requires administrator priviledges for writing).

--- SimpleViewer/MainInterface.cpp
+++ SimpleViewer/MainInterface.cpp
@@ -4,7 +4,7 @@ namespace sv {
 
 	MainInterface::MainInterface(QString openWithFilename, QWidget *parent)
 		: QMainWindow(parent),
-		settings(QDir(QCoreApplication::applicationDirPath()).absoluteFilePath("SimpleViewer.ini"), QSettings::IniFormat) {
+		settings(QSettings::IniFormat, QSettings::SystemScope, "Simple Viewer", "Simple Viewer") {
 
 		setAcceptDrops(true);
 		qRegisterMetaType<cv::Mat>("cv::Mat");
