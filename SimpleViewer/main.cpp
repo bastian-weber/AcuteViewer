4e07dd1483ebdc9cd70fd45adee02cf42393ab1c
Bastian Weber <17008429+bastian-weber@users.noreply.github.com>
2016-08-01 10:27:49 +0200

    Added functionality for opening multiple files.

--- SimpleViewer/main.cpp
+++ SimpleViewer/main.cpp
@@ -3,10 +3,7 @@
 int init(int argc, char* argv[]) {
 	QApplication app(argc, argv);
 
-	QString openWithFilename;
-	if (QCoreApplication::arguments().size() > 1) {
-		openWithFilename = QCoreApplication::arguments().at(1);
-	}
+
 
 	QIcon icon;
 	icon.addFile("./data/icon_16.png");
@@ -19,7 +16,18 @@ int init(int argc, char* argv[]) {
 	icon.addFile("./data/icon_256.png");
 	app.setWindowIcon(icon);
 
-	sv::MainInterface* mainInterface = new sv::MainInterface(openWithFilename);
+	sv::MainInterface* mainInterface;
+
+	if (QCoreApplication::arguments().size() < 2) {
+		mainInterface = new sv::MainInterface(QString());
+	}else if (QCoreApplication::arguments().size() == 2) {
+		mainInterface = new sv::MainInterface(QCoreApplication::arguments().at(1));
+	} else if (QCoreApplication::arguments().size() > 2) {
+		QStringList files = QCoreApplication::arguments();
+		files.pop_front();
+		mainInterface = new sv::MainInterface(files);
+	}
+
 	mainInterface->show();
 
 	return app.exec();
