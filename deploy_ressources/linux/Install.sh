d4c4f96b152cdb4f13d67ce73971bdeb99a397be
Bastian Weber <bastian.weber@uni-weimar.de>
2016-08-04 03:52:31 -0700

    Linux updates.

--- deploy_ressources/linux/Install.sh
+++ deploy_ressources/linux/Install.sh
@@ -9,24 +9,24 @@ fi
 script=$(readlink -f "$0")
 scriptPath=$(dirname "$script")
 
-echo "This script will copy the Simple Viewer application to a location of you choice and create a desktop file."
+echo "This script will copy the Acute Viewer application to a location of you choice and create a desktop file."
 read -e -p "Installation path: " -i "/opt" path
 if [ -d "$path" ]; then
-	installPath="$path/SimpleViewer"
+	installPath="$path/AcuteViewer"
 	uninstallScript="$installPath/Uninstall.sh"
 	if mkdir -p $installPath; then
 		#copy files
 		cp -r "$scriptPath/"* "$installPath"
 		#add symbolic link for use from command line
-		sudo ln -s $installPath/Run_SimpleViewer.sh /usr/bin/simpleviewer
+		sudo ln -s $installPath/Run_AcuteViewer.sh /usr/bin/acuteviewer
 		#write desktop file
-		cat <<- _EOF_ > "/usr/share/applications/SimpleViewer.desktop"
+		cat <<- _EOF_ > "/usr/share/applications/AcuteViewer.desktop"
 		[Desktop Entry]
 		Version=1.0
-		Name=Simple Viewer
-		GenericName=Image Viewer
+		Name=Acute Viewer
+		GenericName=Acute Viewer
 
-		Exec=$installPath/Run_SimpleViewer.sh
+		Exec=$installPath/Run_AcuteViewer.sh
 		Terminal=false
 		Icon=$installPath/data/icon_128.png
 		Type=Application
@@ -41,7 +41,8 @@ if [ -d "$path" ]; then
    			exit 1
 		fi
 		rm -r "$installPath"
-		rm "/usr/share/applications/SimpleViewer.desktop"
+		rm "/usr/share/applications/AcuteViewer.desktop"
+		rm "/usr/bin/acuteviewer"
 		echo "Uninstallation was successful."
 		_EOF_
 		chmod +x "$uninstallScript" 
