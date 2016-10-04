3ea6816bd498e3c8abd1a1e877ad6e411f786e5b
Bastian Weber <17008429+bastian-weber@users.noreply.github.com>
2016-10-04 15:36:30 +0200

    Extended about dialog.

--- AcuteViewer/AboutDialog.cpp
+++ AcuteViewer/AboutDialog.cpp
@@ -8,17 +8,22 @@ namespace sv {
 		this->setSizePolicy(QSizePolicy(QSizePolicy::Fixed, QSizePolicy::Fixed));
 		this->setWindowFlags(this->windowFlags() & ~Qt::WindowContextHelpButtonHint);
 
+		this->setFixedWidth(400);
+
 		this->setWindowTitle(tr("About Acute Viewer"));
 
 		this->majorVersion = AV_MAJOR_VERSION;
 		this->minorVersion = AV_MINOR_VERSION;
 
-		this->infoString = tr("<p>This Version: %1</p>%2<p>Latest Version: %3</p>").arg(QString("%1.%2").arg(this->majorVersion).arg(this->minorVersion));
+		this->infoString = tr("<p><b>This Version: %1</b></p><b>%2</b><p><b>%3</p>").arg(QString("%1.%2").arg(this->majorVersion).arg(this->minorVersion));
 
 		this->infoLabel = new QLabel;
+		this->infoLabel->setTextFormat(Qt::RichText);
+		this->infoLabel->setWordWrap(true);
 
 		this->downloadButton = new QPushButton(tr("Download Now"), this);
-		//QObject::connect(this->downloadButton, SIGNAL(clicked()), this, SLOT(reactToOkButtonClick()));
+		this->downloadButton->setVisible(false);
+		QObject::connect(this->downloadButton, SIGNAL(clicked()), this, SLOT(downloadUpdate()));
 		this->closeButton = new QPushButton(tr("Close"), this);
 		QObject::connect(this->closeButton, SIGNAL(clicked()), this, SLOT(close()));
 
@@ -29,6 +34,7 @@ namespace sv {
 
 		this->mainLayout = new QVBoxLayout;
 		this->mainLayout->addWidget(infoLabel);
+		this->mainLayout->addSpacing(20);
 		this->mainLayout->addLayout(this->buttonLayout);
 
 		this->setLayout(this->mainLayout);
@@ -39,6 +45,7 @@ namespace sv {
 	//============================================================================== PROTECTED ==============================================================================\\
 
 	void AboutDialog::showEvent(QShowEvent * e) {
+		this->downloadButton->setVisible(false);
 		this->infoLabel->setText(this->infoString.arg(this->getInstalledVersion(), "Loading..."));
 		QNetworkRequest request;
 		request.setUrl(QUrl("http://true-contrast.de/avv.txt"));
@@ -67,9 +74,41 @@ namespace sv {
 
 	void AboutDialog::processInfoFile() {
 		if (this->infoDocumentReply->error() == QNetworkReply::NoError) {
-			this->infoLabel->setText(this->infoString.arg(this->getInstalledVersion(), QString(this->infoDocumentReply->readAll().toStdString().c_str())));
+			QString response = QString(this->infoDocumentReply->readAll().toStdString().c_str());
+			this->infoReplyParts = response.split("\\\\\\\\\\");
+			infoDocumentReply->deleteLater();
+			if (this->infoReplyParts.size() >= 4) {
+				this->infoLabel->setText(this->infoString.arg(this->getInstalledVersion(), this->infoReplyParts.at(1).arg(this->infoReplyParts.at(0))));
+				QStringList version = this->infoReplyParts.at(0).split('.');
+				if (version.at(0).toInt() > this->majorVersion || (version.at(1).toInt() > this->minorVersion && version.at(0).toInt() >= this->majorVersion)) {
+					this->downloadButton->setVisible(true);
+				}
+			} else {
+				this->infoLabel->setText(this->infoString.arg(this->getInstalledVersion(), tr("<b>Latest Version: (error)</b>")));
+			}
 		} else {
-			this->infoLabel->setText(this->infoString.arg(this->getInstalledVersion(), tr("(network request failed)")));
+			this->infoLabel->setText(this->infoString.arg(this->getInstalledVersion(), tr("<b>Latest Version: (network request failed)</b>")));
+		}
+	}
+
+	void AboutDialog::downloadUpdate() {
+#ifdef Q_OS_WIN
+		QUrl url(this->infoReplyParts.at(2));
+#else
+		QUrl url(this->infoReplyParts.at(3));
+#endif
+		if (QDesktopServices::openUrl(url)) {
+			QMessageBox::information(this,
+									 QObject::tr("Download Started"),
+									 QObject::tr("The download link has been opened in your default browser."),
+									 QMessageBox::StandardButton::Close,
+									 QMessageBox::StandardButton::Close);
+		} else {
+			QMessageBox::critical(this,
+								  QObject::tr("File Not Moved"),
+								  QObject::tr("The download link could not be opened. Please download the new version manually."),
+								  QMessageBox::StandardButton::Close,
+								  QMessageBox::StandardButton::Close);
 		}
 	}
 
